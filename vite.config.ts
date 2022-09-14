3a26192e32fd62868dbe30b91053a734f7c07326
Innei <tukon479@gmail.com>
2022-09-14 20:17:40 +0800

    fix: ssr optimize
    Signed-off-by: Innei <tukon479@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,22 @@ import { defineConfig, loadEnv } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import pkg from './package.json'
+
 dns.setDefaultResultOrder('verbatim')
+
+const noExternalSet = new Set(Object.keys(pkg.dependencies))
+
+noExternalSet.delete('react')
+noExternalSet.delete('react-dom')
+
 export default ({ mode }) => {
   process.env = loadEnv(mode, process.cwd())
   return defineConfig({
     plugins: [tsconfigPaths(), rakkas(), WindiCSS()],
-    ssr: { noExternal: ['rakkas-seo'] },
+    ssr: {
+      noExternal: [...noExternalSet.values()],
+    },
     define: {
       __DEV__: process.env.NODE_ENV === 'development',
     },
